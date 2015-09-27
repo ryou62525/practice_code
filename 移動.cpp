5a04791f97a8766f7a9dbe045da84c029fb76585
R.komatsu <rkc_tw_st@yahoo.co.jp>
2015-09-27 19:21:26 +0900

    Revert "Revert "移動プログラム""
    This reverts commit 9e443ef22f5fbd3605f8cc3372455a6e6de74944.

---
+++ 移動.cpp
@@ -0,0 +1,70 @@
+﻿
+#include "lib/framework.hpp"
+
+
+
+enum Window 
+{
+  WIDTH  = 512,
+  HEIGHT = 512
+};
+
+struct Player
+{
+	int pos_x, pos_y;
+	int size_x, size_y;
+	int speed;
+};
+
+Player player
+{
+	0, 0,
+	20, 20,
+	1
+};
+
+int Move(AppEnv& env) 
+{
+	if (env.isPressKey('S')) 
+	{
+		player.pos_x += player.speed;
+		return player.pos_x;
+	}
+
+	if (env.isPressKey('A'))
+	{
+		player.pos_x -= player.speed;
+		return player.pos_x;
+	}
+
+	if (env.isPressKey('W'))
+	{
+		player.pos_y += player.speed;
+		return player.pos_y;
+	}
+	
+	if (env.isPressKey('Z'))
+	{
+		player.pos_y -= player.speed;
+		return player.pos_y;
+	}
+}
+
+int main() 
+{
+
+  AppEnv env(Window::WIDTH, Window::HEIGHT);
+
+  while (env.isOpen()) 
+  {
+	  Move(env);
+
+    env.begin();
+    
+	drawFillBox(player.pos_x, player.pos_y, player.size_x, player.size_y, Color::red);
+
+	if (env.isPushKey('Q')){break;}
+
+    env.end();
+  }
+}
